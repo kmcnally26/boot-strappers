de59dd98329b9b161017b35e10323f3578183d92
Kevin McNally <kevinmcnally26@gmail.com>
2015-06-10 19:21:45 +0100

    Update server-eins.sh

--- server-eins.sh
+++ server-eins.sh
@@ -125,6 +125,7 @@ echo Do NOT install any packages/updates for CentOS OS  from the inet
 echo Use only the provided isos until we get pulp up and running.
 
 echo Installing IPA master
+# ipa-server-install --admin-password=password --ds-password=password --hostname=server-eins.ams1.lastminute.com realm=AMS1.LASTMINUTE.COM --domain=ams1.lastminute.com --no-forwarders --setup-dns --no-ntp --idstart=50000 --no --mkhomedir  --unattended
 
 cat /etc/ipa/default
 
