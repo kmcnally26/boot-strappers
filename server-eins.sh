2277099209f4e3cdd23af607c3f4d57e6201384b
Kevin McNally <kevinmcnally26@gmail.com>
2015-06-10 18:12:16 +0100

    Update server-eins.sh

--- server-eins.sh
+++ server-eins.sh
@@ -28,13 +28,6 @@ echo Disable firewall and SElinux
   systemctl disable firewalld && systemctl stop firewalld && iptables -F
 
 ## Execute 
-echo 'At this point in time you need to attach your usb and cp the repos/ directory to /var/www/html/'
-echo "Press y when this is done and I will carry on building this shit? "
-  read -p '#> ' ANSWER
-    if [ ${ANSWER} != y ] ; then
-      echo Aborting
-      exit 1
-    fi
     
 echo 'At this point in time you need to attach your usb and cp the isos to /home/syseng/Downloads/'
 echo "Press y when this is done and I will carry on building this shit? "
@@ -47,13 +40,39 @@ echo "Press y when this is done and I will carry on building this shit? "
 cat << EOF >> /etc/fstab
 ## isos
 /home/syseng/Downloads/CentOS-7-x86_64-Everything-1503-01.iso /var/www/html/repos/centos/7/iso/  iso9660 defaults 0 0
-/home/syseng/Downloads/CentOS-6.6-x86_64-bin-DVD1.iso /var/www/html/repos/centos/6/iso1/         iso9660 defaults 0 0
-/home/syseng/Downloads/CentOS-6.6-x86_64-bin-DVD2.iso /var/www/html/repos/centos/6/iso2          iso9660 defaults 0 0
+#/home/syseng/Downloads/CentOS-6.6-x86_64-bin-DVD1.iso /var/www/html/repos/centos/6/iso1/         iso9660 defaults 0 0
+#/home/syseng/Downloads/CentOS-6.6-x86_64-bin-DVD2.iso /var/www/html/repos/centos/6/iso2          iso9660 defaults 0 0
 
 EOF
 
 echo mounting isos
-mount -a
+mount -a || exit 1
+
+cat << EOF > /etc/yum.repos.d/centos7-dvd.repo
+[centos7-dvd]
+name=centos7 dvd repo
+enabled=1
+baseurl=file:///var/www/html/repos/centos/7/iso/
+gpgcheck=0
+
+EOF
+
+yum clean all
+yum repolist
+
+echo configuring web server for repos
+yum install -y httpd 
+systemctl enable httpd.service
+systemctl restart httpd.service
+
+echo 'At this point in time you need to attach your usb and cp the repos/ directory to /var/www/html/'
+echo "Press y when this is done and I will carry on building this shit? "
+  read -p '#> ' ANSWER
+    if [ ${ANSWER} != y ] ; then
+      echo Aborting
+      exit 1
+    fi
+
 
 cat << EOF > /etc/yum.repos.d/puppet-dependencies.repo
 [puppet-dependencies]
@@ -64,7 +83,7 @@ gpgcheck=0
 
 EOF
 
-cat << EOF > /etc/yum.repos.d/puppet-dependencies.repo
+cat << EOF > /etc/yum.repos.d/puppet-products.repo
 [puppet-products]
 name=puppet products repo
 enabled=1
@@ -73,19 +92,13 @@ gpgcheck=0
 
 EOF
 
-cat << EOF > /etc/yum.repos.d/puppet-dependencies.repo
-[centos7-dvd]
-name=centos7 dvd repo
-enabled=1
-baseurl=file:///var/www/html/repos/centos/7/iso/
-gpgcheck=0
-
-EOF
-
 echo configuring web server for repos
 yum install -y httpd 
 systemctl enable httpd.service
 systemctl restart httpd.service
+
+echo Do NOT install any packages/updates for CentOS OS  from the inet
+echo Use only the provided isos until we get pulp up and running.
 
 exit ${RETVAL}
 # EOF
