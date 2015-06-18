2716ad8a46a6f4ac11f2808b10e3c7e9444f19b9
Kevin McNally <kevinmcnally26@gmail.com>
2015-06-18 16:13:44 +0100

    Update install-basic-puppetmaster.sh

--- install-basic-puppetmaster.sh
+++ install-basic-puppetmaster.sh
@@ -30,8 +30,8 @@ RETVAL=0
 ## Disable firewall and SElinux 
   if !( grep 'SELINUX=disabled' /etc/sysconfig/selinux ); then
     sed 's/SELINUX=[a-z]*/SELINUX=disabled/' /etc/sysconfig/selinux -i 
-    setenforce 0
   fi
+  setenforce 0
 
   systemctl disable firewalld && systemctl stop firewalld && iptables -F
 
