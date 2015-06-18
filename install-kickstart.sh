afe478424c8f7139ae8def947d8660d5177834a5
Kevin McNally <kevinmcnally26@gmail.com>
2015-06-18 16:14:28 +0100

    Update install-kickstart.sh

--- install-kickstart.sh
+++ install-kickstart.sh
@@ -53,8 +53,9 @@ echo "Shall I carry on? "
 echo Disable firewall and SElinux
   if !( grep 'SELINUX=disabled' /etc/sysconfig/selinux ); then
     sed 's/SELINUX=[a-z]*/SELINUX=disabled/' /etc/sysconfig/selinux -i
-    setenforce 0
   fi
+  setenforce 0
+  
   systemctl disable firewalld && systemctl stop firewalld && iptables -F
 
 ## Check network and hostname
